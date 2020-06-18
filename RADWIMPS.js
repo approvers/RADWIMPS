976151628b4d5803131a261ea24ca8b720e0be0e
Naoki Ikeguchi <root@siketyan.dev>
2020-06-18 11:53:23 +0900

    Add EOL lastly

--- RADWIMPS.js
+++ RADWIMPS.js
@@ -1,10 +1,11 @@
 'use strict';
 
+const os = require('os');
 const p = process.stdout.write;
 const f = Promise.prototype.then;
 
 Promise.prototype.then = function () { return f.call(this, () => p('前')); };
-Promise.prototype.世 = function () { return f.call(this, () => p('世')); };
+Promise.prototype.世 = function () { return f.call(this, () => p('世' + os.EOL)); };
 
 Promise.resolve()
     .then()
