94b6a69f38c0622ad3dfa8e21d66f25f89966665
Nanai10a <Nanai10a@tutanota.com>
2020-06-18 18:30:08 +0900

    Create RADWIMPS.java

---
+++ RADWIMPS.java
@@ -0,0 +1,18 @@
+class RADWIMPS {
+    
+    public static void main(String[] args) {
+        RADWIMPS RADWIMPS = new RADWIMPS();
+        
+        RADWIMPS.then().then().then().世();
+        
+    }
+    
+    public void 世() {
+        System.out.print("世");
+    }
+    
+    public RADWIMPS then() {
+        System.out.print("前");
+        return this;
+    }
+}
