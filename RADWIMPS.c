338936cd6faed8f773a423cd694b5976c9bab720
noobest19s <53614631+noobest19s@users.noreply.github.com>
2020-06-18 16:47:20 +0900

    †††

--- RADWIMPS.c
+++ RADWIMPS.c
@@ -2,28 +2,29 @@
 
 
 struct RADWIMPS;
-struct RADWIMPS then();
-struct RADWIMPS 世();
+struct RADWIMPS* then();
+struct RADWIMPS* 世();
 
 
 struct RADWIMPS {
-     struct RADWIMPS (*then)();
-     struct RADWIMPS (*世)();
+    struct RADWIMPS* (*then)();
+    struct RADWIMPS* (*世)();
 };
-struct RADWIMPS RADWIMPS = { then, 世 };
+struct RADWIMPS RADWIMPS = { then, 世};
 
 
-struct RADWIMPS then() {
+struct RADWIMPS* then() {
     printf("前");
-     return RADWIMPS;
+    return &RADWIMPS;
 }
-struct RADWIMPS 世() {
+struct RADWIMPS* 世() {
     printf("世");
-     return RADWIMPS;
+    return &RADWIMPS;
 }
+
 
 int main() {
-    RADWIMPS.then().then().then().世();
+    RADWIMPS.then()->then()->then()->世();
     
     return 0;
 }
