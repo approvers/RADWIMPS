ebdd99c873275a58b7f6598b766e799354804f34
mirror-kt <me@mirror-kt.dev>
2020-06-20 00:57:16 +0900

    修正

--- RADWIMPS.kt
+++ RADWIMPS.kt
@@ -1,9 +1,6 @@
 object RADWIMPS {
-
-​	fun then() = apply { print("前") }
-
-​	fun `世`() = apply{ println("世") }
-
+    fun then() = apply { print("前") }
+    fun `世`() = apply { println("世") }
 }
 
 fun main() = RADWIMPS.then().then().then().`世`()
