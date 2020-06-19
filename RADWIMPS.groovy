4baa19a4163f3077e5c26640e9d9431aaf108f22
sya-ri <sya79lua@gmail.com>
2020-06-19 23:33:35 +0900

    Groovy を追加

---
+++ RADWIMPS.groovy
@@ -0,0 +1,18 @@
+class Main {
+    static class RADWIMPS {
+        def then() {
+            print "前"
+            return this
+        }
+
+        @SuppressWarnings('NonAsciiCharacters')
+        def 世() {
+            print "世"
+            return this
+        }
+    }
+    
+    static void main(String[] args) {
+        new RADWIMPS().then().then().then().世()
+    }
+}
