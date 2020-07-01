3867bde5aa5efa6bbba9365fd63a399781d99e6a
YAMAMOTO Yuji <whosekiteneverfly@gmail.com>
2020-07-01 15:27:53 +0900

    Specify type signatures

--- RADWIMPS.hs
+++ RADWIMPS.hs
@@ -2,8 +2,12 @@ import Prelude hiding ((.))
 
 main :: IO ()
 main = then'().then'().then'().世()
-  where
-    then' :: () -> IO ()
-    then' () = putStr "前"
-    世 () = putStrLn "世"
-    (.) = (>>)
+
+then' :: () -> IO ()
+then' () = putStr "前"
+
+世 :: () -> IO ()
+世 () = putStrLn "世"
+
+(.) :: IO a -> IO b -> IO b
+(.) = (>>)
