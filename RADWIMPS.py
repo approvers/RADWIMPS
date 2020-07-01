ef67d36b35bff4536e0931c48dcd8c169531854a
Takumi Sueda <puhitaku@gmail.com>
2020-07-02 02:11:56 +0900

    Correct signature naming convention
    https://www.python.org/dev/peps/pep-0008/#function-and-method-arguments

--- RADWIMPS.py
+++ RADWIMPS.py
@@ -3,14 +3,14 @@
 
 class RADWIMPS:
     @classmethod
-    def then(self):
+    def then(cls):
         print('前', end='')
-        return self
+        return cls
 
     @classmethod
-    def 世(self):
+    def 世(cls):
         print('世')
-        return self
+        return cls
 
 
 RADWIMPS.then().then().then().世()
