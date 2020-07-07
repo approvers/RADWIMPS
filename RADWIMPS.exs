4f8e5b5cca825105219d469fc3e335984c1f4a0b
sanposhiho <handbomusic@gmail.com>
2020-07-07 15:12:09 +0900

    fix: Formatted with other languages

--- RADWIMPS.exs
+++ RADWIMPS.exs
@@ -3,7 +3,7 @@ defmodule RADWINPS do
 
   def 世(_), do: IO.write("世")
 
-  def main(), do: "君の" |> then() |> then() |> then() |> 世()
+  def main(), do: :RADWIMPS |> then() |> then() |> then() |> 世()
 end
 
 RADWINPS.main()
