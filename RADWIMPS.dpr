4f7da268707100976c418518291a6dc4b8d23a2f
DEKO <deko@ht-deko.minim.ne.jp>
2020-07-08 01:38:57 +0900

    Add Delphi Project File

---
+++ RADWIMPS.dpr
@@ -0,0 +1,26 @@
+﻿{$APPTYPE CONSOLE}
+uses
+  System.SysUtils;
+
+type
+  TRADWIMPS = record
+    function &Then: TRADWIMPS;
+    function 世: TRADWIMPS;
+  end;
+
+  function TRADWIMPS.&Then: TRADWIMPS;
+  begin
+    Write('前');
+    Result := Self;
+  end;
+
+  function TRADWIMPS.世: TRADWIMPS;
+  begin
+    Writeln('世');
+    Result := Self;
+  end;
+
+begin
+  var RADWIMPS: TRADWIMPS;
+  RADWIMPS.&Then.&Then.&Then.世;
+end.
