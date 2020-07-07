e109788fb61a55a17eea1509f411673a768beb6d
USAMI Kenta <tadsan@zonu.me>
2020-07-07 20:51:04 +0900

    Add RADWIMPS.el

---
+++ RADWIMPS.el
@@ -0,0 +1,23 @@
+:;exec emacs --batch -l "$0" -f 前前前世
+;;; RADWIMPS.el --- 君の then-then-then 世は Promise で Future
+
+;;; Code:
+(defsubst then () "前")
+(defsubst 世 () "世")
+
+(defmacro RADWIMPS (&rest lst)
+  "LST."
+  `(mapconcat 'funcall (list ,@(mapcar (lambda (s) (list 'quote s)) lst)) ""))
+
+(defun 前前前世 ()
+  "君を探しはじめたよ."
+  (interactive)
+  (message (RADWIMPS then then then 世)))
+
+(provide 'RADWIMPS)
+
+;; Local Variables:
+;; lexical-binding: t
+;; End:
+
+;;; RADWIMPS.el ends here
