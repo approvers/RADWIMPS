79245ece4fbc2cf9e3ae1385ec02af6ffe9e7f60
sanposhiho <handbomusic@gmail.com>
2020-07-07 15:18:20 +0900

    fix: follow regulations

--- RADWIMPS.exs
+++ RADWIMPS.exs
@@ -1,7 +1,7 @@
 defmodule RADWINPS do
   def then(_), do: IO.write("前")
 
-  def 世(_), do: IO.write("世")
+  def 世(_), do: IO.puts("世")
 
   def main(), do: :RADWIMPS |> then() |> then() |> then() |> 世()
 end
