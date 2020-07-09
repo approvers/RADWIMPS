a5269fb586d8ff167cef48641bb2d8ac8e4d3b49
Phumrapee Limpianchop <git@rayriffy.com>
2020-07-09 16:20:32 +0900

    feat!: add vue

---
+++ RADWIMPS.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <then></then>
+    <then></then>
+    <then></then>
+    <se></se>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+// Self-closing tag is not exist in HTML5 (it is XHTML) https://github.com/vuejs/vue/issues/1036#issuecomment-120634281
+
+const 前 = Vue.component('then', {
+  render: h => h('span', '前')
+})
+
+const 世 = Vue.component('se', {
+  render: h => h('span', '世')
+})
+
+export default Vue.extend({
+  components: {
+    前,
+    世,
+  },
+})
+</script>
