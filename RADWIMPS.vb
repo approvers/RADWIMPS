88f92d5e43b9e7a07899c84baded91bbd6ef13ec
Yusei Ito <yuseikun@Outlook.jp>
2020-07-09 19:13:39 +0900

    Rename 前 to _then

--- RADWIMPS.vb
+++ RADWIMPS.vb
@@ -1,7 +1,7 @@
 public class compiler
 
 class RADWIMPS
-	function 前() as Object
+	function _then() as Object
 		Console.Write ("前")
 		return Me
 	end function
@@ -14,7 +14,7 @@ end class
 
 shared function Main as integer
 Dim radwimps as New RADWIMPS
-radwimps.前.前.前.世
+radwimps._then._then._then.世
 return 0
 End function
 end class
