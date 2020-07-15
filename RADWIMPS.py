d6970b34ad5629628176c9d07b3f1d190c1b8282
shniubobo <shniubobo@outlook.com>
2020-07-15 16:25:57 +0800

    Make use of Future in Python

--- RADWIMPS.py
+++ RADWIMPS.py
@@ -13,4 +13,17 @@ class RADWIMPS:
         return cls
 
 
-RADWIMPS.then().then().then().世()
+async def wait_while_they_play(future):
+    await RADWIMPS.then().then().then().世()
+    future.set_result(True)
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+    future = loop.create_future()
+    loop.create_task(wait_while_they_play(future))
+    await future
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
