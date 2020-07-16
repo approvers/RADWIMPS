e24046e2cfb2c6a3b707453176bf89baeff852e6
shniubobo <shniubobo@outlook.com>
2020-07-16 19:37:31 +0800

    Revert "Make use of asyncio.ensure_future in Python"
    This reverts commit 7cbc6014691306f55154e789032769d78899e067.

--- RADWIMPS.py
+++ RADWIMPS.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import asyncio
 
 
 class RADWIMPS:
@@ -9,14 +8,9 @@ class RADWIMPS:
         return cls
 
     @classmethod
-    async def 世(cls):
+    def 世(cls):
         print('世')
         return cls
 
 
-async def main():
-    await asyncio.ensure_future(RADWIMPS.then().then().then().世())
-
-
-if __name__ == '__main__':
-    asyncio.run(main())
+RADWIMPS.then().then().then().世()
