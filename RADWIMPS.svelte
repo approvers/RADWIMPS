5bc05967156abe4d3d08c97f37c034d94865103a
Manassarn "Noom" Manoonchai <manassarn@gmail.com>
2020-07-22 22:45:22 +0700

    RADWIMPS.svelte
    Make use of Svelte's Reactive Statement `$:`

---
+++ RADWIMPS.svelte
@@ -0,0 +1,15 @@
+<!-- View at : https://svelte.dev/repl/4944a2930773476a9adf047f7a4e5db8 -->
+
+<script>
+	function then() {
+		RADWIMPS = "Then" + RADWIMPS
+		return { then, se: () => RADWIMPS }
+	}
+	
+	$: RADWIMPS = ""
+	$: RADWIMPS += "世"
+</script>
+
+<div>
+	{then().then().then().se()}
+</div>
