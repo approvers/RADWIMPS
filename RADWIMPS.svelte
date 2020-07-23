9a8ac2e99e72cde3d732d88e60ee8297a7eaeabe
Manassarn "Noom" Manoonchai <manassarn@gmail.com>
2020-07-23 13:22:23 +0700

    Use spaces instead of tabs

--- RADWIMPS.svelte
+++ RADWIMPS.svelte
@@ -1,15 +1,15 @@
 <!-- View at : https://svelte.dev/repl/e65e7405d4f54210a372a376c836f7fa -->
 
 <script>
-	function then() {
-		RADWIMPS = "前" + RADWIMPS
-		return { then, 世: () => RADWIMPS }
-	}
-	
-	$: RADWIMPS = ""
-	$: RADWIMPS += "世"
+  function then() {
+    RADWIMPS = "前" + RADWIMPS
+    return { then, 世: () => RADWIMPS }
+  }
+
+  $: RADWIMPS = ""
+  $: RADWIMPS += "世"
 </script>
 
 <div>
-	{then().then().then().世()}
+  {then().then().then().世()}
 </div>
